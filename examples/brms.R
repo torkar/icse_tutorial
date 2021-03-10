71b01a7cd900b81d73cb85a74c06219ccb23170e
Richard Torkar <richard.torkar@gmail.com>
2021-03-10 13:51:02 +0100

    fixes

--- examples/brms.R
+++ examples/brms.R
@@ -6,7 +6,7 @@ brm(
     family = normal(),
     prior = c(
         set_prior(normal(50, 10), class = Intercept),
-        set_prior(normal(-5, 2), class = b,
+        set_prior(lognormal(-5, 2), class = b,
         set_prior(exponential(1), class = sd))
     )
 )
--- examples/foo.jl
+++ examples/foo.jl
@@ -1,16 +1,10 @@
 
 
 @model function linear_regression(x, y)
-    # Set std dev prior.
     σ ~ Exponential(1)
-    
-    # Set intercept prior.
     α ~ Normal(50, 10)
-    
-    # Set the priors on our coefficient.
     βₗ ~ LogNormal(-5, 2)
 
-    # Calculate mu.
     N = length(y)
     for n ∈ 1:N
         μ[n] ~ α + βₗ * x
