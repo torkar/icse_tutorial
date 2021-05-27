31223ec324317ccb37bfa590e0e18a5f186ff93e
Richard Torkar <richard.torkar@gmail.com>
2021-05-27 15:19:04 +0200

    Cleanup

--- foo.R
+++ foo.R
@@ -17,30 +17,29 @@ f <- f[-c(1:5,7:11)]
 # convert Language (factor) to numeric because we hate factors
 f$Language <- as.numeric(f$Language)
 
+# check out the data frame
 str(f)
 
-# Step 1 Likelihood
+##### Step 1 Likelihood
 # ontological and epistemological assumptions?
 
 
-# Step 2 Prior predictive check
+##### Step 2 Prior predictive check
 # Show for grand mean
 
 
-# Step 3 Sampling and diagnostics
+##### Step 3 Sampling and diagnostics
 # grand mean model (m0) and varying intercept model (m1)
 
 
-# Step 4 Posterior predictive checks
+##### Step 4 Posterior predictive checks
 # do a simple postcheck() on m1
 
-# Step 5 Model comparison
+##### Step 5 Model comparison
 # use LOO
 
-
-# Step 6 Compute stuff
+##### Step 6 Compute stuff
 # a) plot posterior values with uncertainty using precis()
 # b) extract samples using extract.samples()
 # c) compute distribution of effect sizes using... arithmetic...
-
 
