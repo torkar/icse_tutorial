746aefa35cfca1044630df6f6222293b8f00f219
Richard Torkar <richard.torkar@gmail.com>
2021-05-27 15:52:22 +0200

    Cleanup

--- foo.R
+++ foo.R
@@ -8,7 +8,7 @@ library(foreign) # for loading data files
 # set working directory
 setwd("~/Development/icse_tutorial/")
 
-# load data file
+# load data file from PROMISE (Sayyad & Menzies), contributed by Shepperd
 f <- read.arff("data/desharnais.arff")
 
 # remove columns we don't need
@@ -23,14 +23,11 @@ str(f)
 ##### Step 1 Likelihood
 # ontological and epistemological assumptions?
 
-
 ##### Step 2 Prior predictive check
 # Show for grand mean
 
-
 ##### Step 3 Sampling and diagnostics
 # grand mean model (m0) and varying intercept model (m1)
-
 
 ##### Step 4 Posterior predictive checks
 # do a simple postcheck() on m1
